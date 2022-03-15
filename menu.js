f130ef073a2ae600bb6a4f4485af81adb4b661e7
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-03-14 18:03:37 -0700

    pag3 finish

--- menu.js
+++ menu.js
@@ -2,4 +2,22 @@ const nav = document.querySelector('.nav');
 
     window.addEventListener('scroll',function(){
         nav.classList.toggle('active', window.scrollY >0)
-    })
+    });
+
+    let listElements = document.querySelectorAll('.list__button--click');
+    listElements.forEach(listElement => {
+        listElement.addEventListener('click', () => {
+            
+            listElement.classList.toggle('arrow');
+            
+            let height = 0;
+            let menu = listElement.nextElementSibling;
+            console.log(menu.scrollHeight)
+            if(menu.clientHeight == '0'){
+                height = menu.scrollHeight;
+            }
+            
+            menu.style.height = `${height}px`
+        })
+        
+    });
